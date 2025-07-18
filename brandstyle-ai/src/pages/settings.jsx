1c0e8e337e849d7d69698a6580dece2d0c4bafbf
maythanhsuaung0-0 <maythanhsuaung1111@gmail.com>
2025-07-17 23:51:15 -0400

    added settings page

---
+++ brandstyle-ai/src/pages/settings.jsx
@@ -0,0 +1,8 @@
+import React, { useState } from 'react';
+const Settings = () => {
+    return(<div>
+        <h2>Settings</h2>
+        <p>Content for Settings tab goes here.</p>
+    </div>)
+}
+export default Settings;
