412496c3a18b91c70becf9bf5e7179c1925ae191
YahyaMohamed3 <yahya11212006@gmail.com>
2025-07-19 16:28:07 -0400

    addec cors

--- backend/main.py
+++ backend/main.py
@@ -1,7 +1,16 @@
 from fastapi import FastAPI
 from routers import auth, users, brands, extract
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # <-- Change to your frontend's URL in prod
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
 
 app.include_router(auth.router, prefix="/api/auth")
 app.include_router(users.router, prefix="/api/users")
